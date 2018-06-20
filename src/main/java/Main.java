d764651ec323ded47beeb25563b6a4dd7d3c5f1f
aks-c <akselcakmak@gmail.com>
2018-06-20 02:47:33 +0200

    Add stages of progression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,3 +20,21 @@ public class Main {
         }
     }
 }
+
+/*
+MVP 1
+- parse a json file with: a grammar, a set of valid symbols, an axiom
+- make the derivations
+- output a valid string of derivations
+
+MVP 2
+- add support for metadata wrt to the blocks (material, etc..) and the shapes (coordinates, sizes, types (cuboids, etc..), etc..)
+- the output is not a string, but some structured data (ie: a JSON file)
+
+MVP 3
+- add utilities/library that converts JSON data into valid sequences of Vanilla Minecraft commands (`/fill`, etc..)
+
+MVP 4
+- Add support for priority levels of derivations (eg: Muller et al, or that other one)
+- Add support for higher levels of details (ie: until now, we could only safely generate valid Mass Models, but with this we'd also have valid façades and maybe valid details on top of that..)
+*/
