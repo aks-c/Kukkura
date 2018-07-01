ab3c900d378704006eaf6bf194a87614461b0b9c
aks-c <akselcakmak@gmail.com>
2018-07-01 21:05:40 +0200

    Handle the coef=0 edge case for random resizing

--- src/main/java/Grammar/Symbol.java
+++ src/main/java/Grammar/Symbol.java
@@ -149,20 +149,24 @@ public class Symbol {
         getSize().setZ(applyRandomResizeToField(CoordinatesUtility.AXIS.Z));
     }
 
-    // TODO: generate the random delta within range to apply
-    // Apply randomized Resize on a specific field.
+    // Apply a randomized Resize on a specific field.
+    //
+    // We want to modify some field.
+    // We apply some random resizing to it, but this resizing needs to be within some defined range.
+    // Call that resizing `delta`.
+    // We define the range delta belongs to, using:
+    // (1) The appropriate coefficient in the ResizeCoefficients triple
+    // (2) The size of our symbol
+    // We want the delta to belong in the following range: [ - coeff * size * 1/2 ; + coeff * size * 1/2 ].
+    // For example, say sx= 10, coeff = 1.
+    // Then the delta will belong to the following range: [-5 ; 5].
+    // Then, after we actually apply the resizing, x will be in the following range: [ 5 ; 15 ].
     private String applyRandomResizeToField(CoordinatesUtility.AXIS axis) {
-        // We want to modify some field.
-        // We apply some random resizing to it, but this resizing needs to be within some defined range.
-        // Call that resizing `delta`.
-        // We define the range delta belongs to, using:
-        // (1) The appropriate coefficient in the ResizeCoefficients triple
-        // (2) The size of our symbol
-        // We want the delta to belong in the following range: [ - coeff * size * 1/2 ; + coeff * size * 1/2 ].
-        // For example, say sx= 10, coeff = 1.
-        // Then the delta will belong to the following range: [-5 ; 5].
-        // Then, after we actually apply the resizing, x will be in the following range: [ 5 ; 15 ].
         int coefficient = Integer.parseInt(getResizeCoefficients().getField(axis));
+        // By convention, if the coefficient is 0, it signifies that there should be no resize.
+        if (coefficient == 0)
+            return getPosition().getField(axis);
+
         int sizeField = Integer.parseInt(getSize().getField(axis));
         int intervalBound = coefficient * sizeField;
         int delta = new Random().nextInt(intervalBound);
