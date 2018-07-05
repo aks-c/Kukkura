71ee291c2df04c02f573d7d4383b9cb8c91d3358
aks-c <akselcakmak@gmail.com>
2018-07-05 22:35:32 +0200

    Add Delta Class
    This is a refactor on the behaviour of deltas and delta coordinates.
    Namely, deltas are not Strings anymore, but a separate object with more
    elaborate behaviour.

--- src/main/java/MetaData/CoordinatesDelta.java
+++ src/main/java/MetaData/CoordinatesDelta.java
@@ -11,23 +11,23 @@ import java.util.ArrayList;
  */
 public class CoordinatesDelta {
     @SerializedName("delta_x")
-    private ArrayList<String> delta_x;
+    private ArrayList<Delta> delta_x;
 
     @SerializedName("delta_y")
-    private ArrayList<String> delta_y;
+    private ArrayList<Delta> delta_y;
 
     @SerializedName("delta_z")
-    private ArrayList<String> delta_z;
+    private ArrayList<Delta> delta_z;
 
-    public ArrayList<String> getDelta_x() {
+    public ArrayList<Delta> getDelta_x() {
         return delta_x;
     }
 
-    public ArrayList<String> getDelta_y() {
+    public ArrayList<Delta> getDelta_y() {
         return delta_y;
     }
 
-    public ArrayList<String> getDelta_z() {
+    public ArrayList<Delta> getDelta_z() {
         return delta_z;
     }
 }
---
+++ src/main/java/MetaData/Delta.java
@@ -0,0 +1,9 @@
+package MetaData;
+
+/**
+ * Created by akselcakmak on 05/07/2018.
+ */
+public class Delta {
+    String delta;
+    String factor;
+}
