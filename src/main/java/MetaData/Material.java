5ad782592343c2bfb04aa462e1686101fe66a558
aks-c <akselcakmak@gmail.com>
2018-07-08 02:46:42 +0200

    Comments

--- src/main/java/MetaData/Material.java
+++ src/main/java/MetaData/Material.java
@@ -6,11 +6,11 @@ import com.google.gson.annotations.SerializedName;
  * Created by akselcakmak on 06/07/2018.
  *
  * Holds the critical information needed to identify a given material, and functions to manipulate that.
+ * Illustrative examples:
+ * - The tuple (mainID="planks", subID="0") represents Oak Wood Planks.
+ * - The tuple (mainID="planks", subID="3") represents Jungle Wood Planks.
  */
 public class Material {
-    // Example representations:
-    // eg: the tuple (mainID="planks", subID="0") represents Oak Wood Planks.
-    // eg: the tuple (mainID="planks", subID="3") represents Jungle Wood Planks.
 
     /**
      * The mainID is the name of the material (i.e. "planks", "dirt", etc..).
